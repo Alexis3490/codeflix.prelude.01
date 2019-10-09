e2b26dda5b229c7b40acad72e198f39f531704c7
Alexis Piraux <alexispiraux@MacBook-Pro-de-Alexis.local>
2019-10-09 15:53:10 +0200

    exercice17-19

---
+++ ex17.js
@@ -0,0 +1,8 @@
+function indexOf(str='', search, fromIndexopt = 0)
+{
+    return str.indexOf(search);
+}
+
+console.log(indexOf('ch0pper', '0'))
+
+console.log(indexOf('ch0pper', 'o'))
---
+++ ex18.js
@@ -0,0 +1,8 @@
+function lastIndexOf(str='', search, fromIndexopt=str.length-1)
+{
+    return str.lastIndexOf(search);
+}
+
+console.log(lastIndexOf('ch0pper', 'p'))
+
+console.log(lastIndexOf('ch0pper', 'o'))
